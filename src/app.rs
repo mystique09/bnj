4fcef6373385a2451ea7041a7f186dd4abe07bde
mystique09 <knightkrypto7@gmail.com>
2024-10-05 13:25:47 +0800

    add: project layout and content

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,7 @@
 use crate::{
-    components::ui::header::Header,
+    components::ui::{
+        container::Container, flex::Flex, grid::Grid, header::Header, title::Heading,
+    },
     error_template::{AppError, ErrorTemplate},
 };
 use leptos::*;
@@ -17,6 +19,9 @@ pub fn App() -> impl IntoView {
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/bnj.css"/>
+        <Stylesheet href="/fonts/space-grotesk/space-grotesk-latin-wght-normal.woff2"/>
+        <Stylesheet href="/fonts/space-mono/space-mono-latin-400-normal.woff2"/>
+        <Stylesheet href="/fonts/space-mono/space-mono-latin-400-normal.woff"/>
 
         // sets the document title
         <Title text="Welcome to Leptos"/>
@@ -44,9 +49,44 @@ pub fn App() -> impl IntoView {
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
+    logging::log!("Hello from HomePage");
+
     view! {
-        <section class="max-w-3xl mx-auto">
-            <h1 class="text-primary font-bold font-mono text-4xl">Hello, world!</h1>
+        <section class="relative w-full h-full max-w-3xl mx-auto">
+            <Grid cols=12 rows=3 gap=6 class="w-full h-full">
+                <Container max_width="[432px]" class="h-[432px] p-6 w-full bg-white rounded-2xl mx-0 shadow-sm col-start-1 col-span-7 row-start-1 row-span-8 shadow-none hover:shadow-lg duration-300 ease-in-out transition-all">
+                    <Flex direction="col".into() gap=4 class="relative w-full h-full">
+                        <Heading class="text-2xl font-bold uppercase text-accent">About Me.</Heading>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </Flex>
+                </Container>
+                <Container max_width="[240px]" class="w-full bg-white h-[240px] mx-0 p-6 rounded-2xl shadow-sm col-start-8 col-span-4 row-start-1 shadow-none hover:shadow-lg duration-300 ease-in-out transition-all">
+                    <Flex direction="col".into() gap=4 class="relative w-full h-full">
+                        <Heading class="text-2xl font-bold uppercase text-accent">Blogs.</Heading>
+                        <p>
+                            Test
+                        </p>
+                    </Flex>
+                </Container>
+                <Container max_width="[432px]" class="w-full bg-white h-[240px] p-6 rounded-2xl shadow-sm row-start-3 col-start-1 col-span-7 shadow-none hover:shadow-lg duration-300 ease-in-out transition-all">
+                    <Flex direction="col".into() gap=4 class="relative w-full h-full">
+                        <Heading class="text-2xl font-bold uppercase text-accent">"Let's connect."</Heading>
+                        <p>
+                            Test
+                        </p>
+                    </Flex>
+                </Container>
+                <Container max_width="[288px]" class="w-full bg-white h-[240px] p-6 rounded-2xl shadow-sm row-start-2 col-start-8 col-span-5 shadow-none hover:shadow-lg duration-300 ease-in-out transition-all">
+                    <Flex direction="col".into() gap=4 class="relative w-full h-full">
+                        <Heading class="text-2xl font-bold uppercase text-accent">Projects</Heading>
+                        <p>
+                            Test
+                        </p>
+                    </Flex>
+                </Container>
+            </Grid>
         </section>
     }
 }
